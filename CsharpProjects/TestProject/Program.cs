bc653fd7efeecd0c9651ec4cdf2aa36676248133
Isadore <remyrascal@live.com>
2025-02-19 21:29:58 -0500

    RandomizeAnimals

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -6,8 +6,18 @@ string[] pettingZoo =
     "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
-//RandAnimals();
+RandomizeAnimals();
 //AssignGroup();
 //PrintGroup(group);
 Console.WriteLine("School A");
 //string[,] group = AssignGroup();
+
+void RandomizeAnimals() {
+    Random rand = new();
+    for(int i = 0; i < pettingZoo.Length; i++) {
+        int r = rand.Next(i, pettingZoo.Length);
+        string t = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = t;
+    }
+}
